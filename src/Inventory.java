ada53a1695df43adea6e6e504e7b6a83b0f51b03
LeeGwangHae <yds05074@naver.com>
2020-09-18 18:56:09 +0900

    player and inventory

--- src/Inventory.java
+++ src/Inventory.java
@@ -1,16 +1,47 @@
 public class Inventory {
-    private int water = 0;
-    private int food = 0;
-    private int wood = 0;
-    private int stone = 0;
+    private Object resource[][] = {{"water", 0},
+                                    {"food",0},
+                                    {"wood",0},
+                                    {"stone",0},
+                                    {"stoneSword",0},
+                                    {"stoneAxe",0},
+                                    {"stonePickAx",0}};
     //counts
 
-    Inventory() {
-
+    Inventory(int water, int food, int wood, int stone, int stoneSword, int stoneAxe, int stonePickAx) {
+        setStone(stone);
+        setFood(food);
+        setStoneAxe(stoneAxe);
+        setStonePickAx(stonePickAx);
+        setStoneSword(stoneSword);
+        setWood(wood);
+        setWater(water);
     }
-    public void _seeInventory(){
-
-    }
-
     //getter & setter
+
+    public Object[][] getResource() {
+        return resource;
+    }
+
+    public void setWater(int water){
+        resource[0][1] = water;
+    }
+    public void setFood(int food){
+        resource[1][1] = food;
+    }
+    public void setWood(int wood){
+        resource[2][1] = wood;
+    }
+    public void setStone(int stone){
+        resource[3][1] = stone;
+    }
+    public void setStoneSword(int stoneSword){
+        resource[4][1] = stoneSword;
+    }
+    public void setStoneAxe(int stoneAxe){
+        resource[5][1] = stoneAxe;
+    }
+    public void setStonePickAx(int stonePickAx){
+        resource[6][1] = stonePickAx;
+    }
 }
