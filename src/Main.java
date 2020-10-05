f057c6fb56805fbc27bda1a078462a9fa214e97a
YJH-W2N2 <yslle0@naver.com>
2020-10-05 21:36:37 +0900

    merge

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         Systems sys = new Systems(50,50);
-        Game Start
+       //Game Start
         sys._dayGame();
 
     }
