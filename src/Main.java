1f794f06408542c9876d284b3bf0751a94f41c5c
YJH-W2N2 <yslle0@naver.com>
2020-10-05 22:10:48 +0900

    asdasd

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Systems sys = new Systems(50,50);
        //Game Start
-
         sys._dayGame();
     }
 }
